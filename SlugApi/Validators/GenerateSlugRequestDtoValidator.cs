e5f3d90523e259238e39cc2c2eaa133d5cc16dda
jalabi <mhmdmh2004@gmail.com>
2026-05-04 14:36:19 +0300

    modify-validation

---
+++ SlugApi/Validators/GenerateSlugRequestDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SlugApi.DTOs;
+
+namespace SlugApi.Validators
+{
+    internal sealed class GenerateSlugRequestDtoValidator : AbstractValidator<GenerateSlugRequest>
+    {
+        public GenerateSlugRequestDtoValidator()
+        {
+            RuleFor(x => x.Text).Must(text => !string.IsNullOrWhiteSpace(text))
+                .WithMessage("Text is required.")
+                .MaximumLength(500)
+                .WithMessage("Text must be less than 500 characters");
+
+            RuleFor(x => x.Separator).Must(c => !c.HasValue || c == '-' || c == '_')
+                .WithMessage("separator must be - or _");
+
+        }
+    }
+}
