88fc2d0168e3bee0ea8179c563c1516a92693877
jalabi <mhmdmh2004@gmail.com>
2026-05-19 15:28:17 +0300

    add-caching

--- SlugApi/Program.cs
+++ SlugApi/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails();
 builder.Services.AddRateLimiterPolicy(builder.Configuration);
+builder.Services.AddMemoryCache();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
